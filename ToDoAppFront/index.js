b75519c6c71bf9a405f44582296f1638c5c7cc2e
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2024-11-19 14:15:31 +0400

    registerjs

---
+++ ToDoAppFront/index.js
@@ -0,0 +1,37 @@
+const signUp = document.getElementById("signup");
+signUp.addEventListener("click", function (event) {
+  event.preventDefault(); 
+  register();
+});
+
+const registerEmail = document.getElementById("registrationemail");
+const registerPassword = document.getElementById("registrationPassword");
+const registerName = document.getElementById("registrationName");
+
+async function register() {
+  try {
+ 
+    let response = await fetch("http://localhost:5001/api/users/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: registerEmail.value,  
+        password: registerPassword.value,
+        username: registerName.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Registration failed!");
+    }
+
+    const data = await response.json();
+    console.log("Registration successful", data);
+ 
+    // window.location.href = "/login.html";
+  } catch (error) {
+    console.error("Error during registration:", error.message);
+  }
+}
